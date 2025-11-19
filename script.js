7aba95e86f4d33304912e27b76b1c81f2c127d42
rafaelaalvys <rafaela.alvys2020@outlook.com>
2025-11-19 20:40:41 -0300

    menu-mobile

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ const navMenu = document.getElementById('nav-menu');
 //Dentro do botão, procura o elemento <i> (ícone da fonte Font awesome)
 const menuIcon = menuBtn.querySelector('i');
 //querySelector()  -> método reservado do javascript usado para buscar
+// o primeiro elemento que corresponde a um seletor CSS dentro de outro elemento.
+
+//Asicionaum "ouvinte de evento" (listener) que detecta o clique no botão do menu.
+menuBtn.addEventListener('click', ()=> {
+    //addEventListener() -> método reservado usado para executar uma função
+    // toda vez que um evento específico ocorre (nesse caso, um clique).
+    //Alterna (liga/desliga) a classe "show" no menu de navegação.
+    //Isso faz o menu aparecer ou desaparecer visualmente via CSS.
+    navMenu.classList.toggle('show');
+    //classList.toggle() -> adiciona a classe se ela não existir; remove se já existir.
+
+    //Troca o ícone (de hambúrguer para "X" e vice-versa), dependendo do estado atual.
+    //Verifica se o ícone atual tem a classe "fa-bars" (ícone do hambúrguer)
+    if (menuIcon.classList.contains ('fa-bars')) {
+        // classList.contains() -> verifica se o elemento contém uma classe específica (retorna true ou false).
+
+        //Se sim, remove a classe do ícone de hambúrguer...
+        menuIcon.classList.remove('fa-bars');
+        // ...e adiciona o ícone de "X" para indicar que o menu pode ser fechado.
+        menuIcon.classList.add('fa-xmark');
+    }
+    else{
+        // Caso contrário volta o ícone para o estado original (hambúrguer).
+        menuIcon.classList.remove('fa-xmark');
+        menuIcon.classList.add('fa-bars');
+    }
+});
